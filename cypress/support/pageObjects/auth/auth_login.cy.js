1d49ebd6207f63090319d6ef119e45e576b952ec
wulanLastia <wulan.lastia@gmail.com>
2022-12-05 08:42:01 +0700

    sidebar_menu

--- cypress/support/pageObjects/auth/auth_login.cy.js
+++ cypress/support/pageObjects/auth/auth_login.cy.js
@@ -15,5 +15,12 @@ export class LoginPage {
     clickBtnLogin(){
         cy.xpath("//button[normalize-space()='Login']").click()
     }
+
+    loginSampleKonseptor(username, password){
+        cy.visit(Cypress.env('base_url'))
+        cy.xpath("//input[@placeholder='Username']").type(username)
+        cy.xpath("//input[@placeholder='Password']").type(password)
+        cy.xpath("//button[normalize-space()='Login']").click()
+    }
     
 }
