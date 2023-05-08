335ad051b9bc4648e26997be671c65c7f01ed483
wulanlastia <wulan.lastia@gmail.com>
2023-05-08 11:26:50 +0700

    kaki surat

--- cypress/e2e/sidebar/konsep_naskah/7_drafting_kaki_surat.cy.js
+++ cypress/e2e/sidebar/konsep_naskah/7_drafting_kaki_surat.cy.js
@@ -30,7 +30,51 @@ before(() => {
 describe('Drafting Kaki Surat Skenario', () => {
     qase(150,
         it('Access kaki surat editing form', () => {
-            menuPage.goToKonsepNaskah()
+            draftingKakiSuratPage.aksesKonsepNaskahSuratBiasa()
+            draftingKakiSuratPage.aksesFormEditingKakiSurat()
+        })
+    )
+
+    qase(151,
+        it('Detail check kaki surat editing form', () => {
+            draftingKakiSuratPage.checkDetail()
+        })
+    )
+
+    qase(154,
+        it('Check penandatangan dropdown list', () => {
+            draftingKakiSuratPage.checkDropdownPenandatangan()
+        })
+    )
+
+    qase(159,
+        it('Leave the field empty when submitting the form', () => {
+            draftingKakiSuratPage.leaveEmptyField()
+        })
+    )
+
+    qase(164,
+        it('Check dropdown list if user select diri sendiri', () => {
+            draftingKakiSuratPage.aksesFormEditingKakiSurat()
+            draftingKakiSuratPage.pilihPenandatanganDiriSendiri()
+        })
+    )
+
+    qase(161,
+        it('Check dropdown list if user select atasan', () => {
+            draftingKakiSuratPage.pilihPenandatanganAtasan()
+        })
+    )
+
+    qase(171,
+        it('Check field if user select pemeriksa', () => {
+            draftingKakiSuratPage.pilihPemeriksa()
+        })
+    )
+
+    qase(201,
+        it('Batal mengisi kaki surat', () => {
+            draftingKakiSuratPage.closeKakiSurat()
         })
     )
 
--- cypress/support/pages/sidebar/konsep_naskah/6_drafting_kaki_surat.cy.js
+++ cypress/support/pages/sidebar/konsep_naskah/6_drafting_kaki_surat.cy.js
@@ -1,15 +1,116 @@
+import kaki_surat from "../../../selectors/sidebar/konsep_naskah/drafting_kaki_surat"
 import { DraftingKonsepNaskahPage } from "./2_drafting_konsep_naskah.cy"
-import { DraftingKopSuratPage } from "./3_drafting_kop_surat.cy"
-import { DraftingKepalaSuratPage } from "./4_drafting_kepala_surat.cy"
 import { MenuPage } from "../menu/menu.cy"
 
 const menuPage = new MenuPage()
 const draftingKonsepNaskahPage = new DraftingKonsepNaskahPage()
-const draftingKopSuratPage = new DraftingKopSuratPage()
-const draftingKepalaSuratPage = new DraftingKepalaSuratPage()
 
 export class DraftingKakiSuratPage {
 
+    aksesKonsepNaskahSuratBiasa() {
+        draftingKonsepNaskahPage.goToKonsepNaskahSuratBiasa()
+        cy.wait(3000)
+    }
 
+    aksesFormEditingKakiSurat() {
+        draftingKonsepNaskahPage.aksesKakiSurat()
+    }
+
+    checkDetail() {
+        const titleKaki = cy.xpath(kaki_surat.titleKaki).as('titleKaki')
+        titleKaki.should('contain', 'Kaki Surat')
+
+        const labelPenandatangan = cy.xpath(kaki_surat.labelPenandatangan).as('labelPenandatangan')
+        labelPenandatangan.should('contain', 'Penandatangan')
+
+        const selectPenandatangan = cy.xpath(kaki_surat.selectPenandatangan).as('selectPenandatangan')
+        selectPenandatangan.find('option:selected').should('have.text', 'Pilih Format Penandatangan')
+
+        const pilihPenandatangan = cy.xpath(kaki_surat.pilihPenandatangan).as('pilihPenandatangan')
+        pilihPenandatangan.should('have.attr', 'placeholder', ' Pilih Penandatangan')
+
+        const labelPemeriksa = cy.xpath(kaki_surat.labelPemeriksa).as('labelPemeriksa')
+        labelPemeriksa.should('contain', 'Pemeriksa')
+
+        const pilihPemeriksa = cy.xpath(kaki_surat.pilihPemeriksa).as('pilihPemeriksa')
+        pilihPemeriksa.should('have.attr', 'placeholder', ' Pilih/ketik nama pemeriksa naskah')
+
+        const btnTambahPemeriksa = cy.xpath(kaki_surat.btnTambahPemeriksa).as('btnTambahPemeriksa')
+        btnTambahPemeriksa.should('be.visible')
+            .should('contain', 'Tambahkan Pemeriksa')
+
+        const labelTembusan = cy.xpath(kaki_surat.labelTembusan).as('labelTembusan')
+        labelTembusan.should('contain', 'Tembusan')
+
+        const subLabelTembusan = cy.xpath(kaki_surat.subLabelTembusan).as('subLabelTembusan')
+        subLabelTembusan.should('contain', 'Tujuan tembusan maksimal 10 orang')
+
+        const pilihTembusan = cy.xpath(kaki_surat.pilihTembusan).as('pilihTembusan')
+        pilihTembusan.should('have.attr', 'placeholder', ' Pilih/ketik tembusan naskah')
+
+        const btnTambahTembusan = cy.xpath(kaki_surat.btnTambahTembusan).as('btnTambahTembusan')
+        btnTambahTembusan.should('be.visible')
+            .should('contain', 'Tambahkan Tembusan')
+    }
+
+    closeKakiSurat() {
+        const closeKaki = cy.xpath(kaki_surat.closeKaki).as('closeKaki')
+        closeKaki.should('be.visible')
+            .click()
+
+        draftingKonsepNaskahPage.validateFormDefault()
+    }
+
+    checkDropdownPenandatangan() {
+        const selectPenandatangan = cy.xpath(kaki_surat.selectPenandatangan).as('selectPenandatangan')
+        selectPenandatangan.select(0).should('have.text', 'Pilih Format Penandatangan Atasan Atas nama Untuk beliau Diri sendiri')
+    }
+
+    leaveEmptyField() {
+        this.closeKakiSurat()
+
+        const btnKirim = cy.get(kaki_surat.btnKirim).as('btnKirim')
+        btnKirim.should('contain', 'Kirim Naskah')
+            .should('have.attr', 'disabled', 'disabled')
+    }
+
+    pilihPenandatanganDiriSendiri() {
+        const selectPenandatangan = cy.xpath(kaki_surat.selectPenandatangan).as('selectPenandatangan')
+        selectPenandatangan.select(4).should('have.value', 'DIRI_SENDIRI')
+
+        const selectedPenandatangan = cy.xpath(kaki_surat.selectedPenandatangan).as('selectedPenandatangan')
+        selectedPenandatangan.should('contain', 'VITA PUTRI UTAMI, S.Sos., M.I.Kom')
+            .invoke('val')
+            .then(text => {
+                const Penandatangan = text;
+                const profileName = cy.xpath(kaki_surat.profileName).as('profileName')
+                profileName.should('contain', Penandatangan)
+            })
+    }
+
+    pilihPenandatanganAtasan() {
+        const selectPenandatangan = cy.xpath(kaki_surat.selectPenandatangan).as('selectPenandatangan')
+        selectPenandatangan.select(1).should('have.value', 'ATASAN')
+
+        const pilihPenandatangan = cy.xpath(kaki_surat.pilihPenandatangan).as('pilihPenandatangan')
+        pilihPenandatangan.type('Hening')
+            .wait(2000)
+            .type('{enter}')
+    }
+
+    pilihPemeriksa() {
+        const pilihPemeriksa = cy.xpath(kaki_surat.pilihPemeriksa).as('pilihPemeriksa')
+        pilihPemeriksa.type('Hening')
+            .wait(2000)
+            .type('{enter}')
+
+        const btnTambahPemeriksa = cy.xpath(kaki_surat.btnTambahPemeriksa).as('btnTambahPemeriksa')
+        btnTambahPemeriksa.click()
+
+        const pilihPemeriksa2 = cy.xpath(kaki_surat.pilihPemeriksa2).as('pilihPemeriksa2')
+        pilihPemeriksa2.type('Fajar')
+            .wait(2000)
+            .type('{enter}')
+    }
 
 }
