a9b452ff7352e7102326a72c08bc0860b94aebba
farrasnaufalwork <farrasnaufalwork@gmail.com>
2023-09-07 16:41:33 +0700

    feat(Nota-Dinas): Create Page Kepala Surat

--- cypress/support/selectors/sidebar/konsep_naskah/nota_dinas/drafting_kepala_surat.js
+++ cypress/support/selectors/sidebar/konsep_naskah/nota_dinas/drafting_kepala_surat.js
@@ -3,6 +3,8 @@ module.exports = {
     titleKepalaSurat: '[data-cy="kepala-surat__nota-dinas__title"]',
     closeKepalaSurat: '[data-cy="kepala-surat__nota-dinas__close-button"]',
 
+    radio1: '[data-cy="radio-button-0"]',
+
     penempatanTujuanSurat: '[data-cy="kepala-surat__nota-dinas__tempat-penulisan-title"]',
 
     titleTujuan: '[data-cy="kepala-surat__tujuan__title"]',
@@ -19,6 +21,7 @@ module.exports = {
 
     labelTembusan: '[data-cy="kepala-surat__nota-dinas__tembusan__title"]',
     inputTembusan0: '[data-cy="kepala-surat__nota-dinas__tembusan__select-option__custom-input0"]',
+    inputTembusan1:'[data-cy="kepala-surat__nota-dinas__tembusan__select-option__custom-input1"]',
     addMoreTembusan: '[data-cy="kepala-surat__nota-dinas__tembusan__add-button"]',
 
     titleKodeKlasifikasi: '[data-cy="kepala-surat__nota-dinas__kode-klasifikasi__title"]',
@@ -29,6 +32,11 @@ module.exports = {
 
     titleTanggal: '[data-cy="kepala-surat__nota-dinas__tanggal-input__title"]',
     inputTanggal: '[data-cy="kepala-surat__tanggal-input"]',
+    scrapTanggalPenomoran: ':nth-child(4) > .col-span-9 > .font-arial',
+    getpopupPenomoran: '[data-cy="dialog-confirmation-ambil-nomor-manual"]',
+    gettitlePopupPenomoran: '[data-cy="dialog__title-confirmation-ambil-nomor-manual"]',
+    getsubtittlePopupPenomoran: '[data-cy="dialog__deskripsi-confirmation-ambil-nomor-manual"]',
+
 
     titleSifatSurat: '[data-cy="kepala-surat__nota-dinas__sifat-surat__title"]',
     selectSifatSurat: '[data-cy="kepala-surat__nota-dinas__sifat-surat__select-option"]',
@@ -38,11 +46,12 @@ module.exports = {
 
     titlePerihal: '[data-cy="kepala-surat__nota-dinas__perihal__title"]',
     inputPerihal: '[data-cy="kepala-surat__nota-dinas__perihal__input"]',
+    scrapPerihal: ':nth-child(8) > .col-span-9 > .font-arial',
 
     scrollForm: '[data-cy="nota-dinas__frame__input-kepala-surat"]',
     previewKepalaLampiran: '[data-cy="surat__kepala-surat"]',
     titlePreviewKepalaSurat: '[data-cy="surat__title__nota-dinas"]',
-
+    
     // XPATH
     radio1: '[data-cy="radio-button-0"]',
     labelRadio1: '[data-cy="radio-label-0"]',
@@ -54,5 +63,10 @@ module.exports = {
     inputTujuanLampiran3: '[data-cy="lampiran-tujuan-surat__select-option__custom-input__3"]',
     inputTujuanLampiran4: '[data-cy="lampiran-tujuan-surat__select-option__custom-input__4"]',
     inputTujuanLampiran5: '[data-cy="lampiran-tujuan-surat__select-option__custom-input__5"]',
+
+    btnkonfirmasiPopupPenomoran: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[9]/div[2]/div[1]/div[1]/div[2]/button[2]',
+    xpathTabelPenomoran1: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[9]/div[1]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[2]',
+    xpathBtnPesanPenomoran1: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[9]/div[1]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[4]/button[1]',
+
 
 }
