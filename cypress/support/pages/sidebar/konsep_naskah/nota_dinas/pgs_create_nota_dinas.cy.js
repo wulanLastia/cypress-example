da4fc8dcee0d305bfb73b6021c77de5c65061957
farrasnaufalwork <farrasnaufalwork@gmail.com>
2023-09-07 16:49:19 +0700

    feat(Nodin): Create Scenario Kepala Surat Internal

---
+++ cypress/support/pages/sidebar/konsep_naskah/nota_dinas/pgs_create_nota_dinas.cy.js
@@ -0,0 +1,109 @@
+import { DraftingNotaDinasPage } from "../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/pgs_drafting_nota_dinas.cy"
+import { DraftingKopSuratNotaDinasPage } from "../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/nodin_drafting_kop_surat.cy"
+import { DraftingKepalaSuratNotaDinasPage } from "../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/nodin_drafting_kepala_surat.cy"
+import { DraftingLampiranSuratPage } from "../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/nodin_drafting_lampiran_surat.cy"
+import { DraftingBadanNaskahPage } from "../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/nodin_drafting_badan_surat.cy.js"
+import { DraftingKakiSuratPage } from "../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/nodin_drafting_kaki_surat.cy.js"
+
+
+const draftingNotaDinasPage = new DraftingNotaDinasPage()
+const draftingKopSuratNotaDinasPage = new DraftingKopSuratNotaDinasPage()
+const draftingKepalaSuratNotaDinasPage = new DraftingKepalaSuratNotaDinasPage()
+const draftingLampiranSuratPage = new DraftingLampiranSuratPage()
+const draftingBadanNaskahPage = new DraftingBadanNaskahPage()
+const draftingKakiSuratPage = new DraftingKakiSuratPage()
+
+export class CreateNotaDinasPage {
+
+    gotoNotaDinas() {
+        draftingNotaDinasPage.goToKonsepNaskahNotaDinas()
+    }
+    
+    createKopSurat() {
+        draftingKopSuratNotaDinasPage.aksesFormEditingKopSurat()
+        draftingKopSuratNotaDinasPage.checkDetail()
+        draftingKopSuratNotaDinasPage.checkPreviewDefault()
+        draftingKopSuratNotaDinasPage.checkPreviewSekda()
+        draftingKopSuratNotaDinasPage.checkPreviewDinas()
+        draftingKopSuratNotaDinasPage.checkPreviewUPTD()
+        draftingKopSuratNotaDinasPage.clickPreviewDinas()
+        draftingKopSuratNotaDinasPage.closeKopSurat()
+    }
+
+    createKepalaSurat() {
+        draftingKepalaSuratNotaDinasPage.aksesFormEditingKepalaSurat()
+        // draftingKepalaSuratNotaDinasPage.checkDetailPreview()
+        // draftingKepalaSuratNotaDinasPage.checkDetail()
+
+        // Pilih Penempatan Daftar Tujuan Surat
+        draftingKepalaSuratNotaDinasPage.clickRButton1KepalaSurat()
+        // Input Tujuan Surat INTERNAL            
+        draftingKepalaSuratNotaDinasPage.inputTujuan("I Gusti Agung Kim Fajar")
+        draftingKepalaSuratNotaDinasPage.addTujuan()
+        draftingKepalaSuratNotaDinasPage.inputTujuanField2("Ludia Rosema")
+        draftingKepalaSuratNotaDinasPage.addTujuan()
+        draftingKepalaSuratNotaDinasPage.inputTujuanField3("Zenal Mustopa")
+        // Input Tembusan Surat INTERNAL
+        draftingKepalaSuratNotaDinasPage.inputTembusan("Raden Andhika")
+        draftingKepalaSuratNotaDinasPage.addTembusan()
+        draftingKepalaSuratNotaDinasPage.inputTembusan2("Upar Suparno")
+        // Field Kode Klasifikasi
+        draftingKepalaSuratNotaDinasPage.inputKodeKlasifikasi("SK (Semua Klasifikasi)")
+        // Field Unit Pengolah
+        draftingKepalaSuratNotaDinasPage.inputUnitPengolah("PAD")
+        // Field Tanggal Penomoran
+        draftingKepalaSuratNotaDinasPage.validateTanggal()
+        // Dropdown Sifat Surat
+        draftingKepalaSuratNotaDinasPage.validateSifatSurat("Penting")
+        // Dropdown Urgensi
+        draftingKepalaSuratNotaDinasPage.validateUrgensi("Amat Segera")
+        // Field Perihal
+        draftingKepalaSuratNotaDinasPage.inputPerihal("Tujuan Kepala Surat - Internal Eksternal - Lampiran")
+        draftingKepalaSuratNotaDinasPage.closeKepalaSurat()
+    }
+
+    createLampiranSurat1() {
+        cy.wait(3000)
+        draftingLampiranSuratPage.aksesFormEditingLampiranSurat()
+        cy.wait(3000)
+        // draftingLampiranSuratPage.checkDetail()
+        // Input Lampiran Surat 1
+        draftingLampiranSuratPage.inputLampiranSurat()
+        draftingLampiranSuratPage.closeLampiran()
+        cy.wait(5000)
+    }
+
+    createLampiranSurat2() {
+        cy.wait(3000)
+        draftingLampiranSuratPage.aksesFormEditingLampiranSuratke2()
+        cy.wait(3000)
+        // Input Lampiran Surat 2
+        draftingLampiranSuratPage.inputLampiranSurat2()
+        draftingLampiranSuratPage.closeLampiran()
+        cy.wait(5000)
+    }
+
+    createBadanSurat() {
+        draftingBadanNaskahPage.inputBadanNaskahProd()
+        draftingBadanNaskahPage.insertDataProd()
+        draftingBadanNaskahPage.closeBadanNaskah()
+
+    }
+
+    createKakiSurat() {
+        draftingKakiSuratPage.aksesFormEditingKakiSurat()
+        draftingKakiSuratPage.inputKakiSurat()
+        // draftingKakiSuratPage.checkDetail()
+        draftingKakiSuratPage.pilihPenandatanganAtasan()
+        draftingKakiSuratPage.pilihPemeriksa()
+        draftingKakiSuratPage.closeKakiSurat()
+    }
+
+    doKirimNaskah() {
+        cy.wait(3000)
+        draftingNotaDinasPage.kirimNaskah({force: true})
+    }
+
+
+
+}
