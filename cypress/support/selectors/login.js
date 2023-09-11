0201f8af96ffdbecc74f92553417480d9aee9ca4
farrasnaufalwork <farrasnaufalwork@gmail.com>
2023-09-11 15:59:17 +0700

    fix(nodin): remove unused element on login

--- cypress/support/selectors/login.js
+++ cypress/support/selectors/login.js
@@ -19,8 +19,6 @@ module.exports = {
     btnLoginSso: '/html[1]/body[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/button[1]',
     inputNip: '/html[1]/body[1]/form[1]/input[1]',
     btnMasuk: '/html[1]/body[1]/form[1]/button[1]',
-    closePopupLandingPageXPATH: '//div[@class="w-full flex items-center justify-center"]//button[@type="button"]',
-    closePopupLandingPageV1UK: '/html[1]/body[1]/div[1]/div[1]/section[2]/div[2]/div[1]/div[1]/div[1]/button[1]',
     goToV2UK: '/html[1]/body[1]/div[1]/aside[1]/div[1]/section[1]/ul[1]/li[27]/a[1]',
     profileUser: '/html[1]/body[1]/div[1]/header[1]/nav[1]/div[1]/ul[1]/li[2]/a[1]',
     btnKeluar: '/html[1]/body[1]/div[1]/header[1]/nav[1]/div[1]/ul[1]/li[2]/ul[1]/li[2]/div[2]/a[1]',
