fd31c482af05fb629a6d9b929e790b94860e826a
farrasnaufalwork <farrasnaufalwork@gmail.com>
2023-09-11 19:25:19 +0700

    fix(nodin): remove input lampiran surat function

--- cypress/support/pages/sidebar/konsep_naskah/surat_biasa/pgs_drafting_surat_biasa.cy.js
+++ cypress/support/pages/sidebar/konsep_naskah/surat_biasa/pgs_drafting_surat_biasa.cy.js
@@ -160,36 +160,6 @@ export class DraftingKonsepNaskahPage {
         titleLampiran.should('contain', 'Lampiran')
     }
 
-    inputLampiranSuratNotaDinas() {
-        const findLampiranPage = cy.xpath(konsep_naskah.previewLampiran).as('findLampiranPage')
-        findLampiranPage.scrollIntoView()
-
-        const previewLampiran1NotaDinas = cy.get(konsep_naskah.previewLampiranNotaDinas).as('previewLampiran1NotaDinas')
-        previewLampiran1NotaDinas.scrollIntoView()
-        .click({ force: true })
-        cy.wait(3000)
-
-
-        const titleLampiran = cy.xpath(konsep_naskah.titleLampiran).as('titleLampiran')
-        titleLampiran.should('contain', 'Lampiran')
-
-    }
-
-    inputLampiran2SuratNotaDinas() {
-        const findLampiranPage = cy.xpath(konsep_naskah.previewLampiran2).as('findLampiranPage')
-        findLampiranPage.scrollIntoView()
-
-        const previewLampiran2NotaDinas = cy.get(konsep_naskah.previewLampiranNotaDinas).as('previewLampiran2NotaDinas')
-        previewLampiran2NotaDinas.scrollIntoView()
-        .click({ force: true })
-        cy.wait(3000)
-
-
-        const titleLampiran = cy.xpath(konsep_naskah.titleLampiran).as('titleLampiran')
-        titleLampiran.should('contain', 'Lampiran')
-
-    }
-
     kirimNaskah() {
         const btnKirimNaskah = cy.get(konsep_naskah.btnKirimNaskah).as('btnKirimNaskah')
         btnKirimNaskah.click()
