bc38e19762095c2d781e5c9cea96eee87cd3df04
farrasnaufalwork <farrasnaufalwork@gmail.com>
2023-09-11 11:33:39 +0700

    fix(nodin): update element and logout function

--- cypress/support/selectors/login.js
+++ cypress/support/selectors/login.js
@@ -9,6 +9,8 @@ module.exports = {
     goToV2: '[data-cy="aksi-login-to-v2"]',
     backToV1: '[data-cy="sidebar__v1__dashboard"]',
     closePopupLandingPage: '[data-cy="dialog__welcoming__button--close"]',
+    getJQueryProfileV2: "button[tabindex='0']",
+
 
     // XPATH
     alertSalah: '/html[1]/body[1]/section[1]/div[1]/div[2]/form[1]/div[1]/div[1]',
@@ -17,6 +19,11 @@ module.exports = {
     btnLoginSso: '/html[1]/body[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/button[1]',
     inputNip: '/html[1]/body[1]/form[1]/input[1]',
     btnMasuk: '/html[1]/body[1]/form[1]/button[1]',
+    closePopupLandingPageXPATH: '//div[@class="w-full flex items-center justify-center"]//button[@type="button"]',
+    closePopupLandingPageV1UK: '/html[1]/body[1]/div[1]/div[1]/section[2]/div[2]/div[1]/div[1]/div[1]/button[1]',
+    goToV2UK: '/html[1]/body[1]/div[1]/aside[1]/div[1]/section[1]/ul[1]/li[27]/a[1]',
     profileUser: '/html[1]/body[1]/div[1]/header[1]/nav[1]/div[1]/ul[1]/li[2]/a[1]',
-    btnKeluar: '/html[1]/body[1]/div[1]/header[1]/nav[1]/div[1]/ul[1]/li[2]/ul[1]/li[2]/div[2]/a[1]'
+    btnKeluar: '/html[1]/body[1]/div[1]/header[1]/nav[1]/div[1]/ul[1]/li[2]/ul[1]/li[2]/div[2]/a[1]',
+    konsepNaskahMenu: '/html[1]/body[1]/div[1]/aside[1]/div[1]/section[1]/ul[1]/li[3]/a[1]/span[1]',
+    showHeaderNav: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/header[1]'
 }
