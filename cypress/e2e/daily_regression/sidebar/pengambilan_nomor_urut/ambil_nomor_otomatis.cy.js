36459bb129335a1c47f382cf1c9f342d5ee2f079
wulanlastia <wulan.lastia@gmail.com>
2023-09-21 11:23:44 +0700

    feat(penomoran): add case ambil nomor otomatis

---
+++ cypress/e2e/daily_regression/sidebar/pengambilan_nomor_urut/ambil_nomor_otomatis.cy.js
@@ -0,0 +1,94 @@
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+import { LoginPage } from "../../../../support/pages/auth/login.cy"
+import { MenuPage } from "../../../../support/pages/sidebar/menu/menu.cy"
+import { PengambilanNomorUrutPage } from "../../../../support/pages/sidebar/pengambilan_nomor_urut/pengambilan_nomor_urut.cy"
+import { AmbilNomorOtomatisPage } from "../../../../support/pages/sidebar/pengambilan_nomor_urut/ambil_nomor_otomatis.cy"
+
+let pengambilanNomorUrutPage = new PengambilanNomorUrutPage()
+let ambilNomorOtomatisPage = new AmbilNomorOtomatisPage()
+let loginPage = new LoginPage()
+let menuPage = new MenuPage()
+let user
+let jenis_naskah
+let uk_up
+
+before(() => {
+    cy.then(Cypress.session.clearCurrentSessionData)
+
+    cy.fixture('cred/credentials_dev.json').then((data) => {
+        user = data
+    })
+
+    cy.fixture('non_cred/penomoran/jenis_naskah.json').then((data) => {
+        jenis_naskah = data
+    })
+
+    cy.fixture('non_cred/penomoran/uk_up.json').then((data) => {
+        uk_up = data
+    })
+})
+
+before(() => {
+    loginPage.loginViaV1(user.nip, user.password)
+    loginPage.directLogin()
+})
+
+after(() => {
+    loginPage.logoutV2step2()
+})
+
+describe('Ambil Nomor Otomatis', { testIsolation: false }, () => {
+
+    qase([1006, 1109, 1012],
+        it('Cek pop up jika mengambil nomor hari ini', () => {
+            // Akses menu pengambilan nomor
+            menuPage.goToPengambilanNomor()
+
+            // Isi seluruh field form
+            pengambilanNomorUrutPage.inputJenisNaskah(jenis_naskah.jenis_naskah1)
+            pengambilanNomorUrutPage.inputUKUP(uk_up.uk_kadispusipda)
+
+            // Cek popup
+            ambilNomorOtomatisPage.checkPopupPengambilanNomor(jenis_naskah.jenis_naskah1, uk_up.uk_kadispusipda)
+        })
+    )
+
+    qase(1016,
+        it('Cek fungsi tombol Batal', () => {
+            ambilNomorOtomatisPage.batalMengambilNomorUrut()
+        })
+    )
+
+    qase(1013,
+        it('Cek detail pop up konfirmasi penomoran naskah otomatis', () => {
+            // Isi seluruh field form
+            pengambilanNomorUrutPage.inputJenisNaskah(jenis_naskah.jenis_naskah1)
+            pengambilanNomorUrutPage.inputUKUP(uk_up.uk_kadispusipda)
+
+            // Cek popup
+            ambilNomorOtomatisPage.checkPopupPengambilanNomor(jenis_naskah.jenis_naskah1, uk_up.uk_kadispusipda)
+
+            // Cek detail
+            ambilNomorOtomatisPage.checkDetailPopupPengambilanNomor()
+        })
+    )
+
+    qase(1014,
+        it('Cek fungsi tombol Dapatkan nomor urut', () => {
+            ambilNomorOtomatisPage.checkPopupDapatkanNomorUrut()
+        })
+    )
+
+    qase(1015,
+        it('Cek fungsi tombol Selesai', () => {
+            ambilNomorOtomatisPage.checkBtnSelesaiMendapatkanNomor()
+        })
+    )
+
+    qase(1017,
+        it('Cek nomor urut', () => {
+            ambilNomorOtomatisPage.checkNomorUrut()
+        })
+    )
+
+})
---
+++ cypress/support/selectors/sidebar/pengambilan_nomor_urut/ambil_nomor_otomatis.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // GET
+    popupPengambilanNomor: '[data-cy="dialog__confirmation-ambil-nomor"]',
+    titlePopupPengambilanNomor: '[data-cy="dialog__confirmation-ambil-nomor__title"]',
+    titlePopupPengambilanNomorDesc: '[data-cy="dialog__confirmation-ambil-nomor__deskripsi"]',
+    labelKonfirmasiPengambilanNomor: '[data-cy="dialog__label-confirmation-ambil-nomor"]',
+    btnCancelPengambilanNomor: '[data-cy="dialog__confirmation-ambil-nomor-manual__button--cancel"]',
+    btnKonfirmasiAmbilNomor: '[data-cy="dialog__confirmation-ambil-nomor-manual__button--submit"]',
+    popupBerhasilMendapatkanNomor: '[data-cy="dialog__success-ambil-nomor"]',
+    labelBerhasilMendapatkanNomor: '[data-cy="dialog__success-ambil-nomor__title"]',
+    descBerhasilMendapatkanNomor: '[data-cy="dialog__success-ambil-nomor__deskripsi"]',
+    btnSelesaiMendapatkanNomor: '[data-cy="dialog__confirmation-ambil-nomor-manual__button--approve"]',
+    validateDataNomorUrut: '[data-cy="table__pengambilan-nomor__item-order-number"]',
+    validateDataTanggalNomorUrut: '[data-cy="table__pengambilan-nomor__item-tanggal-nomor"]',
+    validateDataTanggalPesanNomorUrut: '[data-cy="table__pengambilan-nomor__item-tanggal-dipesan"]',
+    validateStatusNomorUrut: '[data-cy="table__pengambilan-nomor__item-status"]',
+
+    // XPATH
+    xpathLabelTanggalPenomoran: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[5]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/th[1]',
+    xpathLabelJenisNaskah: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[5]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[2]/th[1]',
+    xpathLabelUKUP: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[5]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[3]/th[1]',
+    xpathNomorUrut: '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[15]/div[1]/div[1]/span[1]'
+}
