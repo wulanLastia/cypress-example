c2e9848cc4525bef610d06fadd26ccbcda75c760
wulanlastia <wulan.lastia@gmail.com>
2023-10-11 09:54:58 +0700

    refactor(kotak-masuk) : refactor data-cy

--- cypress/e2e/daily_regression/sidebar/kotak_masuk/1_list_surat_review_naskah.cy.js
+++ cypress/e2e/daily_regression/sidebar/kotak_masuk/1_list_surat_review_naskah.cy.js
@@ -8,13 +8,15 @@ let menuPage = new MenuPage()
 let loginPage = new LoginPage()
 let user
 
+beforeEach(() => {
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
+})
+
 before(() => {
     cy.then(Cypress.session.clearCurrentSessionData)
     cy.fixture('cred/credentials_dev.json').then((data) => {
         user = data
     })
-
-    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
 })
 
 after(() => {
@@ -28,13 +30,46 @@ describe('List Surat Review Naskah Kotak Masuk Skenario', { testIsolation: false
         it('Akses menu kotak masuk (Review naskah)', () => {
             loginPage.loginViaV1(user.nip, user.password)
             loginPage.directLogin()
+            loginPage.directDeployPreview()
             menuPage.goToKotakMasukReviewNaskah()
         })
     )
 
     qase(202,
         it('Cek detail halaman naskah kotak masuk review naskah', () => {
+            cy.wait(3000)
             listSuratReviewNaskahPage.checkDetailHalaman()
+        })
+    )
+
+    qase(232,
+        it('Cek tombol Sebelumnya pada halaman pertama tabel', () => {
+            listSuratReviewNaskahPage.checkPreviousPage()
+        })
+    )
+
+    // Pencarian //
+    qase(204,
+        it('Melakukan pencarian dengan kata kunci perihal yang tersedia pada data', () => {
+            listSuratReviewNaskahPage.searchDokumen('Automation')
+        })
+    )
+
+    qase(203,
+        it('Cek aksi pencarian jika kata kunci kurang dari 3 karakter', () => {
+            listSuratReviewNaskahPage.searchDokumen('Di')
+        })
+    )
+
+    qase(209,
+        it('Input script pada kolom pencarian', () => {
+            listSuratReviewNaskahPage.searchDokumen('<script>alert("hai")</script>')
+        })
+    )
+
+    qase(205,
+        it('Melakukan pencarian dengan kata kunci yang tidak tersedia pada data', () => {
+            listSuratReviewNaskahPage.searchDokumen('XX1234XX')
         })
     )
 })
--- cypress/support/pages/sidebar/kotak_masuk/1_list_surat_review_naskah.cy.js
+++ cypress/support/pages/sidebar/kotak_masuk/1_list_surat_review_naskah.cy.js
@@ -5,22 +5,55 @@ export class ListSuratReviewNaskahPage {
     checkDetailHalaman() {
         this.checkTitleReviewNaskah()
 
-        const filterReviewNaskah = cy.xpath(review_naskah.buttonFilterReviewNaskah).as('filterReviewNaskah')
+        const filterReviewNaskah = cy.get(review_naskah.buttonFilterReviewNaskah).as('filterReviewNaskah')
         filterReviewNaskah.should('contain', 'Atur Filter')
+            .and('be.visible')
 
-        const searchReviewNaskah = cy.xpath(review_naskah.searchReviewNaskah).as('searchReviewNaskah')
-        searchReviewNaskah.should('have.attr', 'placeholder', 'Cari berdasarkan perihal atau tujuan naskah')
+        const searchReviewNaskah = cy.get(review_naskah.searchReviewNaskah).as('searchReviewNaskah')
+        searchReviewNaskah.find('input')
+            .should('have.attr', 'placeholder', 'Cari berdasarkan perihal atau tujuan naskah')
+            .and('be.visible')
 
-        const tableReviewNaskah = cy.xpath(review_naskah.tableReviewNaskah).as('tableReviewNaskah')
+        const tableReviewNaskah = cy.get(review_naskah.tableReviewNaskah).as('tableReviewNaskah')
         tableReviewNaskah.should('have.class', 'w-full mx-auto bg-white rounded-lg mb-24')
+            .and('be.visible')
     }
 
     checkTitleReviewNaskah() {
-        const titleReviewNaskah = cy.xpath(review_naskah.titleReviewNaskah).as('titleReviewNaskah')
+        const titleReviewNaskah = cy.get(review_naskah.titleReviewNaskah).as('titleReviewNaskah')
         titleReviewNaskah.should('contain', 'Kotak Masuk')
+            .and('be.visible')
 
-        const subTitleReviewNaskah = cy.xpath(review_naskah.subTitleReviewNaskah).as('subTitleReviewNaskah')
+        const subTitleReviewNaskah = cy.get(review_naskah.subTitleReviewNaskah).as('subTitleReviewNaskah')
         subTitleReviewNaskah.should('contain', 'Review Naskah')
+            .and('be.visible')
     }
 
+    checkPreviousPage() {
+        const btnPreviousPage = cy.get(review_naskah.btnPreviousPage).as('btnPreviousPage')
+        btnPreviousPage.should('be.disabled')
+    }
+
+    searchDokumen(inputText) {
+        const searchReviewNaskah = cy.get(review_naskah.searchReviewNaskah).as('searchReviewNaskah')
+        searchReviewNaskah.find('input').clear()
+        searchReviewNaskah.type(inputText)
+            .then((inputText) => {
+
+                if (inputText.length > 2) {
+                    const tableEmptyState = cy.get(review_naskah.tableEmptyState).as('tableEmptyState')
+                    tableEmptyState.then($tr => {
+                        if ($tr.is(':visible')) {
+                            closePopup.click()
+                        } else {
+                            const tableReviewNaskah = cy.get(review_naskah.tableReviewNaskah).as('tableReviewNaskah')
+                            tableReviewNaskah.contains('td', inputText)
+                        }
+                    })
+                } else {
+                    const tableDataPerihal = cy.get(review_naskah.tableDataPerihal).as('tableDataPerihal')
+                    tableDataPerihal.should('not.equal', inputText)
+                }
+            })
+    }
 }
