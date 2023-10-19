19c72c4c2eeb12d4404afd71b032632fd573c6af
farrasnaufalwork <farrasnaufalwork@gmail.com>
2023-10-19 16:14:18 +0700

    fix(scenarios_regression): Move All to Newest Path

---
+++ cypress/e2e/daily_regression/sidebar/scenarios_regression/stagging/nota_dinas/skenario3_Penandatangan_Diri_Sendiri.cy.js
@@ -0,0 +1,77 @@
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+import { LoginPage } from "../../../../../../support/pages/auth/login.cy"
+import { MenuPage } from "../../../../../../support/pages/sidebar/menu/menu.cy"
+import { DraftPage } from "../../../../../../support/pages/sidebar/konsep_naskah/konsep_naskah/draft.cy"
+import { DraftingNotaDinasPage } from "../../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/pgs_drafting_nota_dinas.cy"
+import { CreateNotaDinasPage } from "../../../../../../support/pages/sidebar/konsep_naskah/nota_dinas/pgs_create_nota_dinas.cy"
+import { KembalikanNaskahPage } from "../../../../../../support/pages/sidebar/kotak_masuk/3_kembalikan_naskah.cy"
+import { PerbaikiNaskahPage } from "../../../../../../support/pages/sidebar/kotak_masuk/6_perbaiki.cy"
+import { SetujuiPage } from "../../../../../../support/pages/sidebar/kotak_masuk/5_setujui.cy"
+import { KoreksiSuratPage } from "../../../../../../support/pages/sidebar/kotak_masuk/7_koreksi.cy"
+
+
+
+
+let loginPage = new LoginPage()
+let menuPage = new MenuPage()
+let user
+
+let draftPage = new DraftPage()
+let draftingNotaDinasPage = new DraftingNotaDinasPage()
+let createNotaDinasPage = new CreateNotaDinasPage()
+let kembalikanNaskahPage = new KembalikanNaskahPage()
+let perbaikiNaskahPage = new PerbaikiNaskahPage()
+let setujuiPage = new SetujuiPage()
+let koreksiSuratPage = new KoreksiSuratPage()
+
+
+beforeEach(() => {
+    cy.intercept({ resourceType: /xhr/ }, { log: false })
+})
+
+before(() => {
+    cy.then(Cypress.session.clearCurrentSessionData)
+    cy.fixture('cred/credentials_dev.json').then((data) => {
+        user = data
+    })
+})
+
+before(() => {
+    // LogIn Skenario Default
+    loginPage.loginViaV1(user.nip, user.password)
+    loginPage.directLogin()
+
+})
+
+afterEach(() => {
+    cy.wait(10000)
+    loginPage.logoutV2step2()
+})
+
+
+
+describe('Drafting Konsep Naskah Nota Dinas Skenario Penandatangan Diri Sendiri', () => {
+
+    qase([1, 1069, 1064, 1065, 1067, 1066, 1062, 1063, 1061, 721, 723, 724, 725, 1123, 1118, 1146, 1147, 1148, 1151, 1159],
+        it('Nota Dinas Penandatangan Diri Sendiri Tujuan Lampiran Kepala Internal', () => {
+            createNotaDinasPage.gotoNotaDinas() // Cek detail halaman drafting konsep naskah Nota Dinas
+            createNotaDinasPage.createKopSurat()
+            cy.wait(3000)
+            createNotaDinasPage.createLampiranSurat1()
+            cy.wait(3000)
+            createNotaDinasPage.createLampiranSurat2()
+            cy.wait(3000)
+            createNotaDinasPage.createKakiSuratPenandatanganDiriSendiri()
+            cy.wait(3000)
+            createNotaDinasPage.createLampiranKepalaSurat()
+            cy.wait(3000)
+            createNotaDinasPage.createBadanSurat()
+            cy.wait(3000)
+            draftingNotaDinasPage.clickSimpanSurat()
+            cy.wait(3000)
+            draftPage.checkDataPertamaNaskahDisimpan()
+            cy.wait(3000)
+            setujuiPage.doTandaTanganiSurat(user.passphrase)
+        })
+    )
+})
