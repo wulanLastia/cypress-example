8ba1c0cfd91fc05de88aee17030356fdaf3f8baa
Abdurrahman Shofy Adianto <azophy@gmail.com>
2023-10-26 16:57:36 +0700

    fix

--- helpers/feature_toggle_helper.js
+++ helpers/feature_toggle_helper.js
@@ -2,29 +2,31 @@ import * as jose from 'jose'
 
 /* helper method to set Unleash override cookie
  */
-export async function overrideFeatureToggle(cy, toggles = {}, expirationTime = '2h') {
-  const privateKeyString = process.env.UNLEASH_OVERRIDE_PRIVATE_KEY
-  console.log('masuuuuuuuk')
-  console.log({ privateKeyString })
-  const privateKeyAlgorithm = process.env.UNLEASH_OVERRIDE_KEY_ALGORITHM
+export default async function overrideFeatureToggle(toggles = {}, expirationTime = '2h') {
+  const privateKeyString = Cypress.env('UNLEASH_OVERRIDE_PRIVATE_KEY')
+  const privateKeyAlgorithm = Cypress.env('UNLEASH_OVERRIDE_KEY_ALGORITHM')
+
   const privateKey = await jose.importPKCS8(atob(privateKeyString), privateKeyAlgorithm)
 
   const defaultToggles = {
+    // FILL WITH WHATEVER TOGGLE YOU WISH TO SET GLOBALLY
     'EXAMPLE_TOGGLE': true,
   }
+
+  cy.log({ defaultToggles, toggles })
 
   const payload = {
     overrides: { ...defaultToggles, ...toggles },
   }
 
   const jwt = await new jose.SignJWT(payload)
-    .setProtectedHeader({ privateKeyAlgorithm })
+    .setProtectedHeader({ alg: privateKeyAlgorithm })
     .setIssuedAt()
     .setIssuer('cypress-sidebar-digitalservice-id')
     .setAudience('unleash-sidebar-jabarprov-go-id')
     .setExpirationTime(expirationTime)
     .sign(privateKey)
 
-  cy.setCookie('OVERRIDE_FEATURE_TOGGLE', jwt)
+  await cy.setCookie('OVERRIDE_FEATURE_TOGGLE', jwt)
 }
 
