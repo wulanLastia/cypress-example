ae7fab7d0b234c7e99642d190fafb13f09872df3
wulanlastia <wulan.lastia@gmail.com>
2023-11-06 15:57:53 +0700

    fix(surat-biasa): add cy wait

--- cypress/support/pages/sidebar/konsep_naskah/konsep_naskah/pgs_drafting_badan_naskah.cy.js
+++ cypress/support/pages/sidebar/konsep_naskah/konsep_naskah/pgs_drafting_badan_naskah.cy.js
@@ -264,6 +264,7 @@ export class DraftingBadanNaskahPage {
             .should('be.visible')
             .then($el => {
                 $el.innerHTML = textToPaste  // better than `.type()` because you can add formatting
+                cy.wait(3000)
             })
     }
 
