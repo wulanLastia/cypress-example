3f0758294d38f9b3ea9fb7a0a989fb859e87fa73
farrasnaufalwork <farrasnaufalwork@gmail.com>
2023-11-07 13:18:38 +0700

    fix(super): Refactor Data-cy

--- cypress/support/selectors/sidebar/konsep_naskah/surat_perintah/drafting_surat_perintah.js
+++ cypress/support/selectors/sidebar/konsep_naskah/surat_perintah/drafting_surat_perintah.js
@@ -1,19 +1,19 @@
 module.exports = {
     // GET
     btnSuratPerintah: '[data-cy="template-naskah__surat-perintah"]',
-    btnKembali: '[data-cy="surat-surat-perintah-left-arrow"]',
-    titleMenu: '[data-cy="surat-nota-dinas-title"]',
-    selectedKonsep: '[data-cy="surat-biasa-dropdown"]',
+    btnKembali: '[data-cy="surat-perintah__button--back"]',
+    titleMenu: '[data-cy="surat-perintah__title"]',
+    selectedKonsep: '[data-cy="document-type__dropdown"]',
     btnKirimNaskah: '[data-cy="surat-nota-dinas-submit"]',
     previewKop: '[data-cy="surat__kop-surat"]',
     titleKop: '[data-cy="kop-surat__title"]',
-    previewKepala: '[data-cy="surat__kepala-surat"]',
-    titleKepala: '[data-cy="kepala-surat__nota-dinas__title"]',
+    previewKepala: '[data-cy="surat__kepala-surat-preview"]',
+    titleKepala: '[data-cy="surat__title__surat-perintah"]',
     previewBadan: '[data-cy="surat__badan-surat"]',
     titleBadan: '[data-cy="badan-naskah__title"]',
     previewKaki: '[data-cy="surat__kaki-surat"]',
     titleKaki: '[data-cy="kaki-surat__title"]',
-    editFormDefault: '[data-cy="surat-nota-dinas-click-to-edit"]',
+    editFormDefault: '[data-cy="surat-perintah-click-to-edit"]',
     konfirmasiKirimNaskah: '[data-cy="dialog__confirmation-submit__button--kirim-naskah"]',
 
     // XPATH
