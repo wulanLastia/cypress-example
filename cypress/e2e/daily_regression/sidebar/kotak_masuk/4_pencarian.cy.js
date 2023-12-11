f8efdc68f60166b81725d68796af79db1b1164e1
wulanlastia <wulan.lastia@gmail.com>
2023-12-11 14:01:01 +0700

    feat(inbox): refactor and add test case

---
+++ cypress/e2e/daily_regression/sidebar/kotak_masuk/4_pencarian.cy.js
@@ -0,0 +1,64 @@
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+import { LoginPage } from "../../../../support/pages/auth/login.cy"
+import { MenuPage } from "../../../../support/pages/sidebar/menu/menu.cy"
+import { ListSuratReviewNaskahPage } from "../../../../support/pages/sidebar/kotak_masuk/1_list_surat_review_naskah.cy"
+
+let listSuratReviewNaskahPage = new ListSuratReviewNaskahPage()
+let menuPage = new MenuPage()
+let loginPage = new LoginPage()
+let user
+
+beforeEach(() => {
+    cy.intercept({ resourceType: /xhr/ }, { log: false })
+})
+
+before(() => {
+    cy.then(Cypress.session.clearCurrentSessionData)
+    cy.fixture('cred/credentials_dev.json').then((data) => {
+        user = data
+    })
+})
+
+after(() => {
+    qase(411,
+        loginPage.logoutV2step2()
+    )
+})
+
+after(() => {
+    loginPage.logoutV2step2()
+})
+
+describe('Pencarian Naskah Kotak Masuk Skenario', { testIsolation: false }, () => {
+    qase(97,
+        it('Akses menu kotak masuk (Review naskah)', () => {
+            loginPage.loginViaV1(user.nip, user.password)
+            loginPage.directLogin()
+            menuPage.goToKotakMasukReviewNaskah()
+        })
+    )
+
+    qase(204,
+        it('Melakukan pencarian dengan kata kunci perihal yang tersedia pada data', () => {
+            listSuratReviewNaskahPage.searchDokumen('Automation')
+        })
+    )
+
+    qase(203,
+        it('Cek aksi pencarian jika kata kunci kurang dari 3 karakter', () => {
+            listSuratReviewNaskahPage.searchDokumen('Di')
+        })
+    )
+
+    qase(209,
+        it('Input script pada kolom pencarian', () => {
+            listSuratReviewNaskahPage.searchDokumen('<script>alert("hai")</script>')
+        })
+    )
+
+    qase(205,
+        it('Melakukan pencarian dengan kata kunci yang tidak tersedia pada data', () => {
+            listSuratReviewNaskahPage.searchDokumen('XX1234XX')
+        })
+    )
+})
--- cypress/e2e/daily_regression/sidebar/kotak_masuk/6_detail.cy.js
+++ cypress/e2e/daily_regression/sidebar/kotak_masuk/6_detail.cy.js
@@ -4,16 +4,22 @@ import { MenuPage } from "../../../../support/pages/sidebar/menu/menu.cy"
 import { ReviewVerifikasiSuratPage } from "../../../../support/pages/sidebar/kotak_masuk/2_review_verifikasi_surat.cy"
 import { CreateSuratBiasaPage } from "../../../../support/pages/sidebar/konsep_naskah/surat_biasa/pgs_create_surat_biasa.cy"
 
+const { faker } = require('@faker-js/faker')
 let reviewVerifikasiSuratPage = new ReviewVerifikasiSuratPage()
 let menuPage = new MenuPage()
 let loginPage = new LoginPage()
 let createSuratBiasaPage = new CreateSuratBiasaPage()
 let user
+let data_temp
 
 beforeEach(() => {
     cy.then(Cypress.session.clearCurrentSessionData)
     cy.fixture('cred/credentials_dev.json').then((data) => {
         user = data
+    })
+
+    cy.fixture('non_cred/kepala_surat/create_data_surat_biasa.json').then((data) => {
+        data_temp = data
     })
 
     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
@@ -37,9 +43,25 @@ describe('Detail Review dan Verifikasi hasil Surat (Kotak Masuk) Skenario', () =
             menuPage.goToKonsepNaskah()
             createSuratBiasaPage.checkDetail()
             createSuratBiasaPage.inputKopSurat()
-            createSuratBiasaPage.inputKepalaSurat()
-            createSuratBiasaPage.inputKakiSurat()
-            createSuratBiasaPage.inputBadanNaskah()
+            createSuratBiasaPage.inputLampiranSurat(faker.lorem.paragraphs(6, '<br/>\n'))
+            createSuratBiasaPage.inputLampiranSurat2(faker.lorem.paragraphs(6, '<br/>\n'))
+            createSuratBiasaPage.inputKakiSuratSkenario1(
+                data_temp.kaki_surat[0].penandatangan_atasan1,
+                data_temp.kaki_surat[1].pemeriksa1,
+                data_temp.kaki_surat[2].tembusan_internal1,
+                data_temp.kaki_surat[2].tembusan_internal2,
+                data_temp.kaki_surat[2].tembusan_internal3)
+            createSuratBiasaPage.inputKepalaSuratSkenario1(
+                data_temp.kepala_surat[0].tujuan1,
+                data_temp.kepala_surat[0].tujuan2,
+                data_temp.kepala_surat[0].tujuan3,
+                data_temp.kepala_surat[1].lokasi,
+                data_temp.kepala_surat[2].kode_klasifikasi,
+                data_temp.kepala_surat[3].unit_pengolah,
+                data_temp.kepala_surat[4].sifat_surat,
+                data_temp.kepala_surat[5].urgensi_surat,
+                data_temp.kepala_surat[6].perihal2)
+            createSuratBiasaPage.inputBadanNaskahSkenarioRegression(faker.lorem.paragraphs(13, '<br/>\n'))
             createSuratBiasaPage.kirimSurat()
 
             cy.wait(5000)
