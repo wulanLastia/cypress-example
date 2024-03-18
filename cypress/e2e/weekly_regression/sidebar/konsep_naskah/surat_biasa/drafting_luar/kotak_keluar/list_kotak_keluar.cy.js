9ec173e82f95912c7455eed56b5793b928f19e0b
wulanlastia <wulan.lastia@gmail.com>
2024-03-18 15:31:54 +0700

    feat(kotak-keluar) : add test case list kotak keluar

---
+++ cypress/e2e/weekly_regression/sidebar/konsep_naskah/surat_biasa/drafting_luar/kotak_keluar/list_kotak_keluar.cy.js
@@ -0,0 +1,88 @@
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+import { LoginPage } from "../../../../../../../support/pages/auth/login.cy"
+import { KotakKeluarPage } from "../../../../../../../support/pages/sidebar/konsep_naskah/drafting_luar/kotak_keluar.cy"
+
+let kotakKeluarPage = new KotakKeluarPage()
+let loginPage = new LoginPage()
+let user
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Jika terdapat error 'uncaught:exception' pada Headless Mode
+    if (err.message.includes('postMessage')) {
+        return false; // return false digunakan untuk skip error pada Headless Mode
+    }
+
+    // throw error untuk exceptions lain bila terdapat error lainnya selain 'uncaught:exception'
+    throw err;
+});
+
+before(() => {
+    cy.then(Cypress.session.clearCurrentSessionData)
+    cy.fixture('cred/credentials_dev.json').then((data) => {
+        user = data
+    })
+
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: true })
+})
+
+before(() => {
+    loginPage.loginViaV1(user.nip, user.password)
+    loginPage.directLogin()
+})
+
+after(() => {
+    qase(411,
+        loginPage.logoutV2step2()
+    )
+})
+
+describe('Drafting Luar - Test Case List Kotak Keluar', { testIsolation: false }, () => {
+
+    // KOLOM URGENSI
+    qase([3117, 3118, 3119, 3120],
+        it('Cek warna label urgensi', () => {
+            // Go To Kotak Keluar - TTE & Review
+            kotakKeluarPage.goToKotakKeluarTTEReview()
+
+            // Cek warna label
+            kotakKeluarPage.checkWarnaLabelUrgensi()
+        })
+    )
+
+    // KOLOM STATUS
+    qase([], // @TODO : Belum dilakukan penyesuaian di sisi test qase
+        it('Cek warna label status', () => {
+            // Cek kolom status
+            kotakKeluarPage.checkWarnaLabelStatus()
+        })
+    )
+
+    // PENCARIAN
+    qase(3072,
+        it('Cek aksi pencarian jika kata kunci kurang dari 3 karakter', () => {
+            // Input pencarian
+            kotakKeluarPage.searchDokumen('Dr')
+        })
+    )
+
+    qase(3073,
+        it('Melakukan pencarian dengan kata kunci perihal yang tersedia pada data', () => {
+            // Input pencarian
+            kotakKeluarPage.searchDokumen('Automation Drafting')
+        })
+    )
+
+    qase(3074,
+        it('Melakukan pencarian dengan kata kunci yang tidak tersedia pada data', () => {
+            // Input pencarian
+            kotakKeluarPage.searchDokumen('XXX')
+        })
+    )
+
+    qase(3076,
+        it('Input script pada kolom pencarian', () => {
+            // Input pencarian
+            kotakKeluarPage.searchDokumen('<script>alert("hai")</script>')
+        })
+    )
+})
---
+++ cypress/support/selectors/sidebar/konsep_naskah/drafting_luar/kotak_keluar.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // BUTTON
+    btn_menuKotakKeluar: '[data-cy="sidebar-menu-kotak-keluar"]',
+    btn_menuTteReview: '[data-cy="sidebar-menu-kotak-keluar-tte-review"]',
+
+    // LABEL
+    label_tteReviewTitle: '[data-cy="panel__title"]',
+    label_tteReviewSubtitle: '[data-cy="panel__subtitle"]',
+    label_tableHeader0: '[data-cy="table__header-0"]',
+    label_tableHeader1: '[data-cy="table__header-1"]',
+    label_tableHeader2: '[data-cy="table__header-2"]',
+    label_tableHeader3: '[data-cy="table__header-3"]',
+    label_tableheader4: '[data-cy="table__header-4"]',
+    label_tableDataStatus: '[data-cy="tte-review-naskah__table--status-tag-0"]',
+    label_tableDataJenis: '[data-cy="tte-review-naskah__table--jenis-surat-0"]',
+    label_tableDataUrgensi: '[data-cy="tte-review-naskah__table--urgency-tag-0"]',
+    label_tableDataSifat: '[data-cy="tte-review-naskah__table--sifat-0"]',
+    label_tableDataUpdate: '[data-cy="tte-review-naskah__table--update-0"]',
+
+    // INPUT
+    input_searchKotakKeluar: '[data-cy="tte-review__filter-box"]',
+
+    // TABEL
+    table_kotakKeluar: '[data-cy="tte-review-naskah__table"]',
+    table_emptyState: '[data-cy="table__empty-state"]',
+}
