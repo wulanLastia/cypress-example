0858717488a07585f203fb16cf1fdfb6d9286d73
wulanLastia <wulan.lastia@gmail.com>
2024-06-06 10:31:06 +0700

    feat(kotak masuk): add test case detail kotak masuk

---
+++ cypress/e2e/weekly_regression/sidebar/konsep_naskah/surat_biasa/drafting_luar/kotak_masuk/detail_kotak_masuk.cy.js
@@ -0,0 +1,149 @@
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+import { LoginPage } from "@pages/auth/login.cy"
+import { TabRegistrasiPage } from "@pages/sidebar/konsep_naskah/drafting_luar/tab_registrasi.cy"
+import { UploadSingleFilePage } from "@pages/sidebar/konsep_naskah/drafting_luar/pgs_upload_single_file.cy"
+import { TandatanganiPage } from "@pages/sidebar/konsep_naskah/drafting_luar/tandatangani.cy"
+import { KotakKeluarPage } from "@pages/sidebar/konsep_naskah/drafting_luar/kotak_keluar.cy"
+import { KotakMasukPage } from "@pages/sidebar/konsep_naskah/drafting_luar/kotak_masuk.cy"
+
+let uploadSingleFilePage = new UploadSingleFilePage()
+let tabRegistrasiPage = new TabRegistrasiPage()
+let tandatanganiPage = new TandatanganiPage()
+let kotakKeluarPage = new KotakKeluarPage()
+let kotakMasukPage = new KotakMasukPage()
+let loginPage = new LoginPage()
+let user
+let data_temp
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Jika terdapat error 'uncaught:exception' pada Headless Mode
+    if (err.message.includes('postMessage')) {
+        return false; // return false digunakan untuk skip error pada Headless Mode
+    }
+
+    // throw error untuk exceptions lain bila terdapat error lainnya selain 'uncaught:exception'
+    throw err;
+});
+
+before(() => {
+    cy.then(Cypress.session.clearCurrentSessionData)
+
+    cy.fixture('cred/credentials_dev.json').then((data) => {
+        user = data
+    })
+
+    cy.fixture('non_cred/drafting_luar/master_data/create_data.json').then((data) => {
+        data_temp = data
+    })
+
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: true })
+})
+
+afterEach(() => {
+    qase(411,
+        loginPage.logoutV2step2()
+    )
+})
+
+describe('Drafting Luar - Test Case Detail Kotak Masuk', { testIsolation: false }, () => {
+
+    // Create sample data
+    qase([2663, 2708, 2715, 2712, 2754, 2766, 2949, 2952, 2960, 2784, 2785, 2792, 2879, 2867, 2883, 2925, 2777, 2893, 2980, 3016, 2982],
+        it('Upload dan registrasi naskah single file', () => {
+            // Login
+            loginPage.loginViaV1(user.nip_pemeriksa_2_1, user.password)
+            loginPage.directLogin()
+
+            // Go To Konsep Naskah Surat Biasa
+            uploadSingleFilePage.goToUploadSingleFileSuratBiasa()
+
+            // Upload File
+            uploadSingleFilePage.uploadSingleFile(data_temp.upload[0].upload1)
+            uploadSingleFilePage.checkDataFileUpload()
+
+            // Click tab registrasi
+            tabRegistrasiPage.clickTabRegistrasi()
+
+            // Tab Registrasi - Bank Nomor
+            tabRegistrasiPage.inputNomorUrut(data_temp.registrasi[0].bank_nomor_lainnya)
+            tabRegistrasiPage.searchKodeKlasifikasi(data_temp.registrasi[1].kode_klasifikasi)
+            tabRegistrasiPage.inputUnitPengolah(data_temp.registrasi[2].unit_pengolah, data_temp.registrasi[2].unit_pengolah)
+ 
+            // Tab Registrasi - Tujuan Surat
+            tabRegistrasiPage.activateToggleDistribusi()
+            tabRegistrasiPage.inputTujuan(data_temp.env[0].staging, data_temp.registrasi[3].index0, data_temp.registrasi[4].input_internal, data_temp.registrasi[5].tujuan_internal1)
+            tabRegistrasiPage.addMoreTujuan()
+            tabRegistrasiPage.inputTujuan(data_temp.env[0].staging, data_temp.registrasi[3].index1, data_temp.registrasi[4].input_eksternal, data_temp.registrasi[5].tujuan_eksternal1)
+            tabRegistrasiPage.inputTembusan(data_temp.env[0].staging, data_temp.registrasi[3].index0, data_temp.registrasi[4].input_internal, data_temp.registrasi[6].tembusan_internal2)
+            tabRegistrasiPage.addMoreTembusan()
+            tabRegistrasiPage.inputTembusan(data_temp.env[0].staging, data_temp.registrasi[3].index1, data_temp.registrasi[4].input_eksternal, data_temp.registrasi[6].tembusan_eksternal1)
+
+            // Tab Registrasi - Section Identitas Surat
+            const uuid = () => Cypress._.random(0, 1e6)
+            const id = uuid()
+
+            tabRegistrasiPage.inputPerihal('Automation Drafting Luar - Kotak Masuk (Surat Biasa) ' + id, 'Automation Drafting Luar - Kotak Masuk (Surat Biasa) ' + id)
+            tabRegistrasiPage.checkWarnaLabelUrgensi(data_temp.registrasi[7].urgensi_surat, data_temp.registrasi[3].index0)
+            tabRegistrasiPage.inputSifat(data_temp.registrasi[8].sifat_surat1)
+
+            // Tab Registrasi - Section Penandatangan
+            tabRegistrasiPage.addMorePenandatangan()
+            tabRegistrasiPage.inputPenandatanganDiriSendiri()
+            tabRegistrasiPage.addMorePenandatangan()
+            tabRegistrasiPage.inputPenandatanganAtasan(data_temp.registrasi[9].atasan2, data_temp.env[0].staging)
+
+            // Upload file pengantar
+            tabRegistrasiPage.uploadSuratPengantar(data_temp.upload[0].upload1)
+            tabRegistrasiPage.checkDataFileUpload()
+
+            // Melakukan TTE Naskah (Penandatangan Diri Sendiri)
+            tandatanganiPage.tandatanganiNaskah()
+            tandatanganiPage.checkInputDataRegistrasi()
+            tandatanganiPage.tteNaskah()
+            tandatanganiPage.submitTteNaskah(user.passphrase, data_temp.env[0].staging)
+        })
+    )
+
+    // Begin test case detail kotak masuk
+    qase([3389, 3390, 3406, 3393, 3395, 3396, 3397, 3398, 3399, 3400, 3401, 3394],
+        it.only('Detail kotak masuk', () => {
+            // Login 
+            loginPage.loginViaV1(user.nip_pemeriksa_2_2, user.password)
+            loginPage.directLogin()
+            
+            // Go To Kotak Masuk - TTE & Review 3389
+            kotakMasukPage.goToKotakMasukTTEReview()
+
+            // Cek status pada detail halaman detail kotak masuk review naskah 3390
+            kotakMasukPage.checkNaskahKotakMasuk(data_temp.env[0].staging)
+            kotakMasukPage.checkStatusdanDetailNaskah()
+
+            // Cek tombol tandatangani jika penerima atasan 3406
+            kotakMasukPage.checkBtnTte()
+
+            // Cek detail data tab registrasi - jenis naskah 3395
+            kotakMasukPage.checkDataJenisNaskah()
+
+            // Cek detail data tab registrasi - nomor naskah 3396
+            kotakMasukPage.checkDataNomorNaskah()
+
+            // Cek detail data tab registrasi - urgensi 3397
+            kotakMasukPage.checkDataUrgensi()
+
+            // Cek detail data tab registrasi - perihal 3398
+            kotakMasukPage.checkDataPerihal()
+
+            // Cek detail data tab registrasi - sifat 3399
+            kotakMasukPage.checkDataSifat()
+
+            // Cek detail data tab registrasi - penerima 3400
+            kotakMasukPage.checkDataPenerima()
+
+            // Cek detail data tab registrasi - konseptor 3401
+            kotakMasukPage.checkDataPenandatangan()
+
+            // Cek tombol kembali 3394
+            kotakMasukPage.checkBtnKembali()
+        })
+    )
+})
