ef25fd124eb8c508330edb2a506cd158e869639b
wulanLastia <wulan.lastia@gmail.com>
2024-06-11 09:36:22 +0700

    feat(kotak keluar): add test case detail kotak keluar

---
+++ cypress/e2e/weekly_regression/sidebar/konsep_naskah/surat_biasa/drafting_luar/kotak_keluar/detail_kotak_keluar.cy.js
@@ -0,0 +1,167 @@
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+import { LoginPage } from "@pages/auth/login.cy"
+import { TabRegistrasiPage } from "@pages/sidebar/konsep_naskah/drafting_luar/tab_registrasi.cy"
+import { UploadSingleFilePage } from "@pages/sidebar/konsep_naskah/drafting_luar/pgs_upload_single_file.cy"
+import { TandatanganiPage } from "@pages/sidebar/konsep_naskah/drafting_luar/tandatangani.cy"
+import { KotakKeluarPage } from "@pages/sidebar/konsep_naskah/drafting_luar/kotak_keluar.cy"
+
+let uploadSingleFilePage = new UploadSingleFilePage()
+let tabRegistrasiPage = new TabRegistrasiPage()
+let tandatanganiPage = new TandatanganiPage()
+let kotakKeluarPage = new KotakKeluarPage()
+let loginPage = new LoginPage()
+let user
+let data_temp
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Jika terdapat error 'uncaught:exception' pada Headless Mode
+    if (err.message.includes('postMessage')) {
+        return false; // return false digunakan untuk skip error pada Headless Mode
+    }
+
+    // throw error untuk exceptions lain bila terdapat error lainnya selain 'uncaught:exception'
+    throw err;
+});
+
+before(() => {
+    cy.then(Cypress.session.clearCurrentSessionData)
+
+    cy.fixture('cred/credentials_dev.json').then((data) => {
+        user = data
+    })
+
+    cy.fixture('non_cred/drafting_luar/master_data/create_data.json').then((data) => {
+        data_temp = data
+    })
+
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: true })
+})
+
+/*after(() => {
+    qase(411,
+        loginPage.logoutV2step2()
+    )
+})*/
+
+describe('Drafting Luar - Test Case Detail Kotak Keluar', { testIsolation: false }, () => {
+
+    // Create sample data
+    qase([2663, 2708, 2715, 2712, 2754, 2766, 2949, 2952, 2960, 2784, 2785, 2792, 2879, 2867, 2883, 2925, 2777, 2893, 2980, 3016, 2982],
+        it('Upload dan registrasi naskah single file', () => {
+            // Login
+            loginPage.loginViaV1(user.nip_pemeriksa_2_1, user.password)
+            loginPage.directLogin()
+
+            // // Go To Konsep Naskah Surat Biasa
+            // uploadSingleFilePage.goToUploadSingleFileSuratBiasa()
+
+            // // Upload File
+            // uploadSingleFilePage.uploadSingleFile(data_temp.upload[0].upload1)
+            // uploadSingleFilePage.checkDataFileUpload()
+
+            // // Click tab registrasi
+            // tabRegistrasiPage.clickTabRegistrasi()
+
+            // // Tab Registrasi - Bank Nomor
+            // tabRegistrasiPage.inputNomorUrut(data_temp.registrasi[0].bank_nomor_lainnya)
+            // tabRegistrasiPage.searchKodeKlasifikasi(data_temp.registrasi[1].kode_klasifikasi)
+            // tabRegistrasiPage.inputUnitPengolah(data_temp.registrasi[2].unit_pengolah, data_temp.registrasi[2].unit_pengolah)
+ 
+            // // Tab Registrasi - Tujuan Surat
+            // tabRegistrasiPage.inputTujuanTembusanSurat()
+
+            // // Tab Registrasi - Section Identitas Surat
+            // const uuid = () => Cypress._.random(0, 1e6)
+            // const id = uuid()
+
+            // tabRegistrasiPage.inputPerihal('Automation Drafting Luar - Kotak Keluar (Surat Biasa) ' + id, 'Automation Drafting Luar - Kotak Keluar (Surat Biasa) ' + id)
+            // tabRegistrasiPage.checkWarnaLabelUrgensi(data_temp.registrasi[7].urgensi_surat, data_temp.registrasi[3].index0)
+            // tabRegistrasiPage.inputSifat(data_temp.registrasi[8].sifat_surat1)
+
+            // // Tab Registrasi - Section Penandatangan
+            // tabRegistrasiPage.addMorePenandatangan()
+            // tabRegistrasiPage.inputPenandatanganDiriSendiri()
+            // tabRegistrasiPage.addMorePenandatangan()
+            // tabRegistrasiPage.inputPenandatanganAtasan(data_temp.registrasi[9].atasan2, data_temp.env[0].staging)
+
+            // // Upload file pengantar
+            // tabRegistrasiPage.uploadSuratPengantar(data_temp.upload[0].upload1)
+            // tabRegistrasiPage.checkDataFileUpload()
+
+            // // Melakukan TTE Naskah (Penandatangan Diri Sendiri)
+            // tandatanganiPage.tandatanganiNaskah()
+            // tandatanganiPage.checkInputDataRegistrasi()
+            // tandatanganiPage.tteNaskah()
+            // tandatanganiPage.submitTteNaskah(user.passphrase, data_temp.env[0].staging)
+
+            // cy.wait(3000)
+        })
+    )
+
+    // Begin test case detail kotak keluar
+    qase(3205,
+        it('Akses halaman kotak keluar TTE dan Review', () => {
+            // Akses halaman kotak keluar TTE dan Review
+            kotakKeluarPage.goToKotakKeluarTTEReview()
+
+            // Akses detail naskah kotak keluar
+            kotakKeluarPage.checkNaskahKotakKeluar(data_temp.env[0].staging)
+        })
+    )
+
+    qase(3206,
+        it('Cek detail halaman detail kotak keluar review naskah', () => {
+            // Cek detail halaman detail kotak keluar review naskah
+            kotakKeluarPage.checkDetailNaskah()
+        })
+    )
+
+    qase(3252,
+        it('Cek detail data tab registrasi - jenis naskah', () => {
+            // Cek detail data tab registrasi - jenis naskah
+            kotakKeluarPage.checkDataJenisNaskah()
+        })
+    )
+
+    qase(3253,
+        it('Cek detail data tab registrasi - nomor naskah', () => {
+            // Cek detail data tab registrasi - nomor naskah
+            kotakKeluarPage.checkDataNomorNaskah()
+        })
+    )
+
+    qase(3254,
+        it('Cek detail data tab registrasi - urgensi', () => {
+            // Cek detail data tab registrasi - urgensi
+            kotakKeluarPage.checkDataUrgensi()
+        })
+    )
+
+    qase(3255,
+        it('Cek detail data tab registrasi - perihal', () => {
+            // Cek detail data tab registrasi - perihal
+            kotakKeluarPage.checkDataPerihal()
+        })
+    )
+
+    qase(3256,
+        it('Cek detail data tab registrasi - sifat', () => {
+            // Cek detail data tab registrasi - sifat
+            kotakKeluarPage.checkDataSifat()
+        })
+    )
+
+    qase(3257,
+        it('Cek detail data tab registrasi - penerima', () => {
+            // Cek detail data tab registrasi - penerima
+            kotakKeluarPage.checkDataPenerima()
+        })
+    )
+
+    qase(3258,
+        it('Cek detail data tab registrasi - penandatangan', () => {
+            // Cek detail data tab registrasi - penandatangan
+            kotakKeluarPage.checkDataPenandatangan()
+        })
+    )
+})
--- cypress/support/pages/sidebar/konsep_naskah/drafting_luar/kotak_keluar.cy.js
+++ cypress/support/pages/sidebar/konsep_naskah/drafting_luar/kotak_keluar.cy.js
@@ -17,6 +17,92 @@ export class KotakKeluarPage {
             .click({force: true})
     }
 
+    checkDetailHalamanKotakKeluar() {
+        // Assertion page
+        const label_tteReviewTitle = cy.get(kotak_keluar.label_tteReviewTitle).as('label_tteReviewTitle')
+        label_tteReviewTitle.should('contain', 'Kotak Keluar')
+
+        const label_tteReviewSubtitle = cy.get(kotak_keluar.label_tteReviewSubtitle).as('label_tteReviewSubtitle')
+        label_tteReviewSubtitle.should('contain', 'TTE & Review')
+
+        const input_searchKotakKeluar = cy.get(kotak_keluar.input_searchKotakKeluar).as('input_searchKotakKeluar')
+        input_searchKotakKeluar.should('be.visible')
+
+        // @TODO: Assert tombol atur filter
+
+        const label_tableHeader0 = cy.get(kotak_keluar.label_tableHeader0).as('label_tableHeader0')
+        label_tableHeader0.find('span')
+            .should('contain', 'Status naskah')
+
+        const label_tableHeader2 = cy.get(kotak_keluar.label_tableHeader2).as('label_tableHeader2')
+        label_tableHeader2.find('span')
+            .should('contain', 'Urgensi')
+
+        const label_tableHeader1 = cy.get(kotak_keluar.label_tableHeader1).as('label_tableHeader1')
+        label_tableHeader1.find('span')
+            .should('contain', 'Jenis naskah, perihal & nomor naskah')
+
+        const label_tableHeader3 = cy.get(kotak_keluar.label_tableHeader3).as('label_tableHeader3')
+        label_tableHeader3.find('span')
+            .should('contain', 'Sifat')
+
+        const label_tableheader4 = cy.get(kotak_keluar.label_tableheader4).as('label_tableheader4')
+        label_tableheader4.find('span')
+            .should('contain', 'Aksi terakhir')
+
+        const table_btnPrevPage = cy.get(kotak_keluar.table_btnPrevPage).as('table_btnPrevPage')
+        table_btnPrevPage.scrollIntoView()
+            .should('be.visible')
+    }
+
+    checkDataUrgensi() {
+        cy.readFile(getPreviewData).then((object) => {
+            const urgensiValue = object.identitas_surat[1].urgensi
+
+            const label_tableDataUrgensi = cy.get(kotak_keluar.label_tableDataUrgensi).as('label_tableDataUrgensi')
+            label_tableDataUrgensi.find('p')
+            .should('contain', urgensiValue)
+        })
+    }
+
+    checkDataJenis() {
+        cy.readFile(getPreviewData).then((object) => {
+            const jenisNaskahValue = object.upload_file[0].jenis_naskah
+
+            const label_tableDataJenis = cy.get(kotak_keluar.label_tableDataJenis).as('label_tableDataJenis')
+            label_tableDataJenis.should('contain', jenisNaskahValue)
+        })
+    }
+
+    checkDataSifat() {
+        cy.readFile(getPreviewData).then((object) => {
+            const sifatValue = object.identitas_surat[2].sifat
+
+            const label_tableDataSifat = cy.get(kotak_keluar.label_tableDataSifat).as('label_tableDataSifat')
+            label_tableDataSifat.should('contain', sifatValue)
+        })
+    }
+
+    checkDataPerihal() {
+        cy.readFile(getPreviewData).then((object) => {
+            const perihalValue = object.identitas_surat[0].perihal
+
+            const label_tableDataJenis = cy.get(kotak_keluar.label_tableDataJenis).as('label_tableDataJenis')
+            label_tableDataJenis.find('p')
+                .should('contain', perihalValue)
+        })
+    }
+
+    checkDataNomorNaskah() {
+        cy.readFile(getPreviewData).then((object) => {
+            const nomorUrutValue = object.bank_nomor[1].nomor_urut
+
+            const label_tableDataJenis = cy.get(kotak_keluar.label_tableDataJenis).as('label_tableDataJenis')
+            label_tableDataJenis.find('p')
+                .should('contain', nomorUrutValue)
+        })
+    }
+
     checkWarnaLabelUrgensi() {
         // Get label existing on data ke 0
         const label_tableDataUrgensi = cy.get(kotak_keluar.label_tableDataUrgensi).as('label_tableDataUrgensi')
@@ -106,14 +192,160 @@ export class KotakKeluarPage {
             })
     }
 
-    checkNaskahKotakKeluar() {
+    checkNaskahKotakKeluar(inputEnv) {
         cy.readFile(getPreviewData).then((object) => {
-            // Assert perihal naskah (Status tidak diassert karena kemungkinan akan cepat proses ttenya)
             const perihal = object.identitas_surat[0].perihal
 
-            const label_tableDataJenis = cy.get(kotak_keluar.label_tableDataJenis).as('label_tableDataJenis')
-            label_tableDataJenis.find('p')
-                .should('contain', perihal)
+            if(inputEnv == "staging"){
+                cy.intercept('POST', Cypress.env('base_url_api_v2')).as('checkResponse')
+
+                const input_searchKotakKeluar = cy.get(kotak_keluar.input_searchKotakKeluar).first().as('input_searchKotakKeluar')
+                input_searchKotakKeluar.find('input')
+                    .clear()
+                    .type(perihal)
+
+                cy.wait('@checkResponse', { timeout: 10000 })
+                    .then((interception) => {
+                        if (interception.response.statusCode === 200) {
+                            const label_tableDataJenis = cy.get(kotak_keluar.label_tableDataJenis).first().as('label_tableDataJenis')
+                            label_tableDataJenis.contains('p', perihal)
+                                .click()
+                        }
+                    })
+            }else{
+                const input_searchKotakKeluar = cy.get(kotak_keluar.input_searchKotakKeluar).first().as('input_searchKotakKeluar')
+                input_searchKotakKeluar.find('input')
+                    .clear()
+                    .type(perihal)
+
+                // Wait until document found
+                cy.wait(10000)
+
+                const label_tableDataJenis = cy.get(kotak_keluar.label_tableDataJenis).first().as('label_tableDataJenis')
+                label_tableDataJenis.contains('p', perihal)
+                    .click()
+            }   
+        })
+    }
+
+    checkDetailNaskah() {
+        // Assertion Detail 
+        const tab_registrasi = cy.get(kotak_keluar.tab_registrasi).as('tab_registrasi')
+        tab_registrasi.should('have.class', 'tabs__menu active')
+
+        const tab_history = cy.get(kotak_keluar.tab_history).as('tab_history')
+        tab_history.should('have.class', 'tabs__menu')
+
+        const btn_previewPdf = cy.get(kotak_keluar.btn_previewPdf).as('btn_previewPdf')
+        btn_previewPdf.should('be.visible')
+
+        cy.readFile(getPreviewData).then((object) => {
+            const jenisNaskahValue = object.upload_file[0].jenis_naskah
+
+            const label_detailHeaderJenisNaskah = cy.get(kotak_keluar.label_detailHeaderJenisNaskah).as('label_detailHeaderJenisNaskah')
+            label_detailHeaderJenisNaskah.should('contain', jenisNaskahValue)
+        
+            const urgensiValue = object.identitas_surat[1].urgensi
+
+            const label_detailHeaderUrgensi = cy.get(kotak_keluar.label_detailHeaderUrgensi).as('label_detailHeaderUrgensi')
+            label_detailHeaderUrgensi.should('contain', urgensiValue)
+        })
+
+        // @TODO: Icon status TTE Naskah
+    }
+
+    checkDataJenisNaskah() {
+        cy.readFile(getPreviewData).then((object) => {
+            const jenisNaskahValue = object.upload_file[0].jenis_naskah
+
+            const label_detailJenisNaskah = cy.get(kotak_keluar.label_detailJenisNaskah).as('label_detailJenisNaskah')
+            label_detailJenisNaskah.scrollIntoView().should('contain', 'Jenis Naskah')
+
+            const tab_dataJenisNaskah = cy.get(kotak_keluar.tab_dataJenisNaskah).as('tab_dataJenisNaskah')
+            tab_dataJenisNaskah.should('contain', jenisNaskahValue)
+        })
+    }
+
+    checkDataNomorNaskah() {
+        cy.readFile(getPreviewData).then((object) => {
+            const nomorUrutValue = object.bank_nomor[1].nomor_urut
+
+            const label_detailNomorUrut = cy.get(kotak_keluar.label_detailNomorUrut).as('label_detailNomorUrut')
+            label_detailNomorUrut.scrollIntoView().should('contain', 'Nomor Naskah')
+
+            const tab_dataNomorUrut = cy.get(kotak_keluar.tab_dataNomorUrut).as('tab_dataNomorUrut')
+            tab_dataNomorUrut.should('contain', nomorUrutValue)
+        })
+    }
+
+    checkDataUrgensi() {
+        cy.readFile(getPreviewData).then((object) => {
+            const urgensiValue = object.identitas_surat[1].urgensi
+
+            const label_detailUrgensi = cy.get(kotak_keluar.label_detailUrgensi).as('label_detailUrgensi')
+            label_detailUrgensi.scrollIntoView().should('contain', 'Urgensi')
+
+            const tab_dataUrgensi = cy.get(kotak_keluar.tab_dataUrgensi).as('tab_dataUrgensi')
+            tab_dataUrgensi.find('p')
+                .should('contain', urgensiValue)
+        })
+    }
+
+    checkDataPerihal() {
+        cy.readFile(getPreviewData).then((object) => {
+            const perihalValue = object.identitas_surat[0].perihal
+
+            const label_detailPerihal = cy.get(kotak_keluar.label_detailPerihal).as('label_detailPerihal')
+            label_detailPerihal.scrollIntoView().should('contain', 'Perihal')
+
+            const tab_dataPerihal = cy.get(kotak_keluar.tab_dataPerihal).as('tab_dataPerihal')
+            tab_dataPerihal.should('contain', perihalValue)
+        })
+    }
+
+    checkDataSifat() {
+        cy.readFile(getPreviewData).then((object) => {
+            const sifatValue = object.identitas_surat[2].sifat
+
+            const label_detailSifat = cy.get(kotak_keluar.label_detailSifat).as('label_detailSifat')
+            label_detailSifat.scrollIntoView().should('contain', 'Sifat')
+
+            const tab_dataSifat = cy.get(kotak_keluar.tab_dataSifat).as('tab_dataSifat')
+            tab_dataSifat.should('contain', sifatValue)
+        })
+    }
+
+    checkDataPenerima() {
+        cy.readFile(getPreviewData).then((object) => {
+            const label_detailPenerimaTitle = cy.get(kotak_keluar.label_detailPenerimaTitle).as('label_detailPenerimaTitle')
+            label_detailPenerimaTitle.scrollIntoView().should('contain', 'Penerima')
+
+            const label_detailPenerimaDescription = cy.get(kotak_keluar.label_detailPenerimaDescription).as('label_detailPenerimaDescription')
+            label_detailPenerimaDescription.scrollIntoView().should('contain', 'Naskah ini akan didistribusikan langsung ke akun Sidebar penerima')
+
+            const tujuanInternalValue = object.tujuan_surat[0].tujuan_internal
+            const arrTujuanInternal = tujuanInternalValue.split(" (")
+
+            const tab_dataPenerima0 = cy.get(kotak_keluar.tab_dataPenerima0).as('tab_dataPenerima0')
+            tab_dataPenerima0.contains(arrTujuanInternal[0], { matchCase: false })
+        })
+    }
+
+    checkDataPenandatangan() {
+        cy.readFile(getPreviewData).then((object) => {
+            const label_detailPenandatanganTitle = cy.get(kotak_keluar.label_detailPenandatanganTitle).as('label_detailPenandatanganTitle')
+            label_detailPenandatanganTitle.scrollIntoView().should('contain', 'Penandatangan')
+
+            const penandatanganDiriSendiriValue = object.penandatangan[0].penandatangan_diri_sendiri
+
+            const tab_dataPenandatangan0 = cy.get(kotak_keluar.tab_dataPenandatangan0).as('tab_dataPenandatangan0')
+            tab_dataPenandatangan0.contains(penandatanganDiriSendiriValue, { matchCase: false })
+
+            const penandatanganAtasanValue = object.penandatangan[1].penandatangan_atasan
+            const arrPenandatanganAtasan = penandatanganAtasanValue.split(" (")
+
+            const tab_dataPenandatangan1 = cy.get(kotak_keluar.tab_dataPenandatangan1).as('tab_dataPenandatangan1')
+            tab_dataPenandatangan1.contains(arrPenandatanganAtasan[0], { matchCase: false })
         })
     }
 }
