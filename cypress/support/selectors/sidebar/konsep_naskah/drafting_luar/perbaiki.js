6af73656f6557ed51ebe86dba39f5d5c88625c9e
wulanLastia <wulan.lastia@gmail.com>
2024-07-24 17:57:24 +0700

    feat(tab-perbaikan): add test case tab perbaikan

---
+++ cypress/support/selectors/sidebar/konsep_naskah/drafting_luar/perbaiki.js
@@ -0,0 +1,36 @@
+module.exports = {
+    // TAB
+    tab_perbaikan: '[data-cy="tab__detail-revision"]',
+
+    // BUTTON
+    btn_perbaikiNavbar: '[data-cy="tte-review-naskah__button--go-to-document-revision"]',
+
+    // LABEL
+    label_pemeriksaTitle: '[data-cy="pemeriksa__title"]',
+    label_pemeriksaName: '[data-cy="pemeriksa-data__card-user__people-name"]',
+    label_pemeriksaPosition: '[data-cy="pemeriksa-data__card-user__people-position"]',
+    label_notePerbaikanTitle: '[data-cy="revision-note__title"]',
+    label_notePerbaikanDescription: '[data-cy="revision-note__description"]',
+    label_poinPerbaikanTitle: '[data-cy="revision-point__title"]',
+    label_inputPerihal: '[data-cy="revision-point__data-0__description"]',
+    label_inputIsiNaskah: '[data-cy="revision-point__data-1__description"]',
+    label_inputLampiran: '[data-cy="revision-point__data-2__description"]',
+    label_inputTujuanNaskah: '[data-cy="revision-point__data-3__description"]',
+    label_inputAlamatNaskah: '[data-cy="revision-point__data-4__description"]',
+    label_inputTembusan: '[data-cy="revision-point__data-5__description"]',
+    label_inputUrgensiNaskah: '[data-cy="revision-point__data-6__description"]',
+    label_inputSifatNaskah: '[data-cy="revision-point__data-7__description"]',
+    label_inputKodeKlasifikasi: '[data-cy="revision-point__data-8__description"]',
+    
+
+    // CHECKBOX
+    cb_inputPerihal: '[data-cy="revision-point__data-0__icon"]',
+    cb_inputIsiNaskah: '[data-cy="revision-point__data-1__icon"]',
+    cb_inputLampiran: '[data-cy="revision-point__data-2__icon"]',
+    cb_inputTujuanNaskah: '[data-cy="revision-point__data-3__icon"]',
+    cb_inputAlamatNaskah: '[data-cy="revision-point__data-4__icon"]',
+    cb_inputTembusan: '[data-cy="revision-point__data-5__icon"]',
+    cb_inputUrgensiNaskah: '[data-cy="revision-point__data-6__icon"]',
+    cb_inputSifatNaskah: '[data-cy="revision-point__data-7__icon"]',
+    cb_inputKodeKlasifikasi: '[data-cy="revision-point__data-8__icon"]',
+}
