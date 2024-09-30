fa20102f8a7c4b09343857cce007a7ec470d5307
wulanLastia <wulan.lastia@gmail.com>
2024-09-30 17:14:31 +0700

    refactor: update test scenario sekda, asda, kepala biro

--- cypress/e2e/daily_regression/stagging/drafting_luar/scenario_khusus/skenario51_sekda_sekretaris_daerah.cy.js
+++ cypress/e2e/daily_regression/stagging/drafting_luar/scenario_khusus/skenario51_sekda_sekretaris_daerah.cy.js
@@ -4,13 +4,11 @@ import { TabRegistrasiPage } from "@pages/sidebar/konsep_naskah/drafting_luar/ta
 import { UploadSingleFilePage } from "@pages/sidebar/konsep_naskah/drafting_luar/pgs_upload_single_file.cy"
 import { TandatanganiPage } from "@pages/sidebar/konsep_naskah/drafting_luar/tandatangani.cy"
 import { KotakKeluarPage } from "@pages/sidebar/konsep_naskah/drafting_luar/kotak_keluar.cy"
-import { HistoriPage } from "@pages/sidebar/konsep_naskah/drafting_luar/histori.cy"
 
 let uploadSingleFilePage = new UploadSingleFilePage()
 let tabRegistrasiPage = new TabRegistrasiPage()
 let tandatanganiPage = new TandatanganiPage()
 let kotakKeluarPage = new KotakKeluarPage()
-let historiPage = new HistoriPage()
 let loginPage = new LoginPage()
 let user
 let data_temp
@@ -39,7 +37,7 @@ before(() => {
     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
 })
 
-describe('Drafting Luar - Skenario Asisten Sekretariat Daerah', { testIsolation: false }, () => {
+describe('Drafting Luar - Skenario Sekretariat Daerah (Sekretaris Daerah)', { testIsolation: false }, () => {
 
     qase([79, 4289, 3885, 3879, 3884, 4229, 4054, 4061, 3996, 4122, 4126, 3930, 4008, 4134, 4141, 4138],
         it('Upload dan registrasi naskah single file', () => {
@@ -60,21 +58,17 @@ describe('Drafting Luar - Skenario Asisten Sekretariat Daerah', { testIsolation:
             // Tab Registrasi - Bank Nomor 3884
             tabRegistrasiPage.inputPenomoran()
  
-            // Tab Registrasi - Tujuan Surat Asisten Sekretariat Daerah 4229, 4054, 4061
+            // Tab Registrasi - Tujuan Surat Sekretariat Daerah (Sekretaris Daerah) 4229, 4054, 4061
             tabRegistrasiPage.inputTujuanTembusan()
             tabRegistrasiPage.selectLintasDinas()
-            tabRegistrasiPage.inputTujuan(data_temp.env[0].staging, data_temp.registrasi[3].index0, data_temp.registrasi[4].input_internal, data_temp.registrasi[5].tujuan_asisten_sekda1)
-            tabRegistrasiPage.addMoreTujuan()
-            tabRegistrasiPage.inputTujuan(data_temp.env[0].staging, data_temp.registrasi[3].index1, data_temp.registrasi[4].input_internal, data_temp.registrasi[5].tujuan_asisten_sekda2)
-            tabRegistrasiPage.addMoreTujuan()
-            tabRegistrasiPage.inputTujuan(data_temp.env[0].staging, data_temp.registrasi[3].index2, data_temp.registrasi[4].input_internal, data_temp.registrasi[5].tujuan_asisten_sekda3)
+            tabRegistrasiPage.inputTujuan(data_temp.env[0].staging, data_temp.registrasi[3].index0, data_temp.registrasi[4].input_internal, data_temp.registrasi[5].tujuan_sekda1)
             tabRegistrasiPage.simpanTujuanTembusan()
 
             // Tab Registrasi - Section Identitas Surat 3996, 4122, 4126
             const uuid = () => Cypress._.random(0, 1e6)
             const id = uuid()
 
-            tabRegistrasiPage.inputPerihal('Testing Skenario E2E Tujuan Asisten Sekretariat Daerah ' + id, 'Testing Skenario E2E Tujuan Asisten Sekretariat Daerah ' + id)
+            tabRegistrasiPage.inputPerihal('Testing Skenario E2E Tujuan Sekretariat Daerah (Sekretaris Daerah) ' + id, 'Testing Skenario E2E Tujuan Sekretariat Daerah (Sekretaris Daerah) ' + id)
             tabRegistrasiPage.checkWarnaLabelUrgensi(data_temp.registrasi[7].urgensi_surat, data_temp.registrasi[3].index0)
             tabRegistrasiPage.inputSifat(data_temp.registrasi[8].sifat_surat3)
 
